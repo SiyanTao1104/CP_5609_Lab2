75806840751444d629f83726aa7f8ce72de5cc8d
SiyanTao1104 <114071083@qq.com>
2022-08-27 17:40:13 +0800

    Add main method in Vector class

--- VectorBoardObjects.cpp
+++ VectorBoardObjects.cpp
@@ -1,5 +1,82 @@
 //
 // Created by TSY on 2022/8/27.
 //
-
+#include <iostream>
 #include "VectorBoardObjects.h"
+#include "WallH.h"
+#include "Bomb.h"
+
+using namespace std;
+VectorBoardObjects:: VectorBoardObjects(p_BoardObject newOne):list()
+{
+    std::vector <p_BoardObject>:: iterator it;
+    it=list.begin();
+    list.insert(it,newOne);
+}
+
+p_BoardObject VectorBoardObjects:: theFirst()
+{
+    if (0 < list.size()  )
+    { internalIterator=list.begin();
+        return (*internalIterator);
+    }
+    else
+        return NULL;
+}
+
+
+void VectorBoardObjects:: addOne(p_BoardObject newOne)
+{
+    std::vector <p_BoardObject>:: iterator it;
+    it=list.begin();
+    list.insert(it,newOne);
+}
+
+p_BoardObject VectorBoardObjects :: theNext()
+{
+    internalIterator++;
+    if (internalIterator < list.end())
+    {
+        return (*internalIterator);
+    }
+    else
+        return NULL;
+}
+
+void VectorBoardObjects :: removeOnePlayer()
+{
+    std::vector <p_BoardObject>:: iterator it;
+    it=list.begin();
+    while (it<list.end())
+    { if (A_PLAYER==(*it)->get_Type())
+        { list.erase(it); return;}
+        it++;
+    }
+
+    return ;
+}
+
+
+bool VectorBoardObjects :: isThereABomb()
+{
+    std::vector <p_BoardObject>:: iterator it;
+    it=list.begin();
+    while (it<list.end())
+    { if (A_BOMB==(*it)->get_Type())
+        {  return true;}
+        it++;
+    }
+
+    return false ;
+}
+
+int main(){
+    
+    VectorBoardObjects vectorBoardObjects = new Player(4,6);
+
+    vectorBoardObjects.theFirst()->get_Type()
+
+
+
+    return 0;
+}
--- VectorBoardObjects.h
+++ VectorBoardObjects.h
@@ -1,13 +1,29 @@
 //
 // Created by TSY on 2022/8/27.
 //
+#include <vector>
+#include  "BoardObjectH.h"
+#include  "PlayerH.h"
+#include  "CookieH.h"
 
 #ifndef CP_5609_LAB2_VECTORBOARDOBJECTS_H
 #define CP_5609_LAB2_VECTORBOARDOBJECTS_H
 
+typedef BoardObject* p_BoardObject;
+
 
 class VectorBoardObjects {
+    std::vector <p_BoardObject> list;
+    std::vector < p_BoardObject>::iterator internalIterator;
+public:
+    VectorBoardObjects(p_BoardObject theFirst);
 
+    p_BoardObject theFirst();
+    p_BoardObject theNext();
+    void addOne(p_BoardObject newOne);
+    void removeOnePlayer();
+    bool isThereABomb();
+    int isCookieValue();
 };
 
 
