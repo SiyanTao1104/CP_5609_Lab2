55198de47c2a3cee0441a2ee3e149cd1b9ce5ab7
SiyanTao1104 <114071083@qq.com>
2022-08-27 17:39:58 +0800

    Add related code in monster class

--- Monster.cpp
+++ Monster.cpp
@@ -2,4 +2,13 @@
 // Created by TSY on 2022/8/27.
 //
 
+
+#include "BoardObjectH.h"
 #include "Monster.h"
+
+void Monster :: touch ()
+{
+            set_color(BLUE);
+
+    //std::cerr << " Player Object touched\n";
+}
--- Monster.h
+++ Monster.h
@@ -4,11 +4,25 @@
 
 #ifndef CP_5609_LAB2_MONSTER_H
 #define CP_5609_LAB2_MONSTER_H
+#include <iostream>
 
 
-class Monster {
+#include "BoardObjectH.h"
+class Monster : public BoardObject
+{
 
+private:
+    ObjectColours color1;
+    ObjectColours  color2;
+
+public:
+    Monster (int start_x, int start_y):BoardObject(start_x,start_y)
+    {
+        color1 = RED;
+        color2  = BLUE;
+        myClass=A_MONSTER;
+    };
+
+    virtual void touch ();
 };
-
-
 #endif //CP_5609_LAB2_MONSTER_H
