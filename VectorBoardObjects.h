fc7c882217b49bc7feab700bfa32cc8a89b7d7af
SiyanTao1104 <114071083@qq.com>
2022-08-27 16:41:55 +0800

    Add new VectorBoardObjects class

---
+++ VectorBoardObjects.h
@@ -0,0 +1,14 @@
+//
+// Created by TSY on 2022/8/27.
+//
+
+#ifndef CP_5609_LAB2_VECTORBOARDOBJECTS_H
+#define CP_5609_LAB2_VECTORBOARDOBJECTS_H
+
+
+class VectorBoardObjects {
+
+};
+
+
+#endif //CP_5609_LAB2_VECTORBOARDOBJECTS_H
